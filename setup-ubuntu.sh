3f352c6fabac03dc30af2c7459ce76d5fe2b32d9
ITLackey <itlackey@gmail.com>
2019-12-12 12:57:14 -0600

    improved deb setup

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 ./scripts/git-install-deb.sh
+./scripts/git-configure.sh
+./scripts/teamsinstall-deb.sh
+./scripts/azure-data-studio-install-deb.sh
+./scripts/dotnet-sdk-install-deb.sh
 ./setup-snap.sh
