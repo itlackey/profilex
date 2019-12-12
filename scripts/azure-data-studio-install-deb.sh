f800d5008093cde863c04dc0e64347a23b673f0f
ITLackey <itlackey@gmail.com>
2019-12-12 12:30:44 -0600

    added azure data studio

---
+++ scripts/azure-data-studio-install-deb.sh
@@ -0,0 +1,8 @@
+FILENAME="azuredatastudio-linux-1.13.1.deb"
+
+echo "Downloading Azure Data Studio deb file"
+wget "https://azuredatastudiobuilds.blob.core.windows.net/releases/1.13.1/$FILENAME"
+echo "Installing Azure Data Studio"
+sudo apt install ./$FILENAME
+rm "$FILENAME"
+echo "Azure Data Studio installed"
