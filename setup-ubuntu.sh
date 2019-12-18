e0d2fc86d2247f26eb7928fb05281e6e09e33a80
ITLackey <itlackey@gmail.com>
2019-12-18 12:59:29 -0600

    added fira code font

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -5,6 +5,7 @@ sudo apt update
 ./scripts/git-install-deb.sh
 ./scripts/git-configure.sh
 ./scripts/docker-install-deb.sh
+./scripts/fira-code-install-deb.sh
 ./scripts/teamsinstall-deb.sh
 ./scripts/azure-data-studio-install-deb.sh
 ./scripts/dotnet-sdk-install-deb.sh
